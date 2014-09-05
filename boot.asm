9e3e7118ac34c8241346a4a31f7138af5df2657a
CN-Zxcv <1032980965@qq.com>
2014-09-05 17:16:20 +0800

    boot test

---
+++ boot.asm
@@ -0,0 +1,29 @@
+;%define __DEBUG__
+
+%ifdef	__DEBUG__
+	org	0100h
+%else
+	org	07c00h
+%endif
+
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
+	call	DispStr
+	jmp	$
+
+DispStr:
+	mov	ax, BootMessage
+	mov	bp, ax
+	mov	cx, 16
+	mov	ax, 01301h
+	mov	bx, 000ch
+	mov	dl, 0
+	int	10h
+	ret
+
+BootMessage: db	"Is this work?"
+
+times	510-($-$$) db 0
+
+dw	0xaa55
